b781b444ad120eb811c54ab168007a596182a5d5
Riuzzo01 <davide1.mariuzzo@gmail.com>
2024-05-20 14:51:22 +0200

    minor tweaks

--- Iterative_methods/Jacobi_method.rb
+++ Iterative_methods/Jacobi_method.rb
@@ -1,7 +1,8 @@
 require_relative '../utils.rb'
+require 'csv'
 
-def jacobi_method(matrix, b, iter)
-
+def jacobi_method()
+    file = CSV.read("spa1.mtx", col_sep: " ")
 end
 
 jacobi_method()
---
+++ Iterative_methods/csv_converter.py
@@ -0,0 +1,9 @@
+import csv
+import gzip
+import os
+import scipy.io
+import pandas
+
+
+
+mat = scipy.io.mmread("spa1.mtx")
